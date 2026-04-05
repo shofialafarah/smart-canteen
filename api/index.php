ade34f8b6d5b3e94a1cf9b69e20cd3a8914e34b4
shofialafarah <shofia.lafarah74@gmail.com>
2026-04-05 07:20:13 +0700

    Tes deploy

--- api/index.php
+++ api/index.php
@@ -1,8 +1,26 @@
 <?php
 
-// Arahkan ke file autoload dan app di folder root
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Vercel Serverless Fix
+|--------------------------------------------------------------------------
+| Vercel hanya mengizinkan penulisan file di folder /tmp. 
+| Kita arahkan storage Laravel ke sana agar tidak error saat compile view.
+*/
+$storagePath = '/tmp/storage/framework/views';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0755, true);
+}
+
+// Set path storage ke /tmp
+$app->useStoragePath('/tmp/storage');
+
+// Penting: Beritahu Laravel di mana letak folder view yang baru dicompile
+config(['view.compiled' => $storagePath]);
+// ------------------------------------------------------------------------
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
